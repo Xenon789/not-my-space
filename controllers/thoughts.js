0e07f4942d9d5637d58d26c4d39fed55e9c70a22
BuildTools <unconfigured@null.spigotmc.org>
2023-02-27 07:01:53 -0800

    completed user controllers. working on api and routes

--- controllers/thoughts.js
+++ controllers/thoughts.js
@@ -144,7 +144,7 @@ const removeReaction = (req, res) => {
 
 module.exports = {
     getThoughts,
-    getSingleThought,
+    getOneThought,
     createThought,
     updateThought,
     deleteThought,
--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,130 @@
 const { User, Thought } = require('../models');
 
+// function to get all users
 const getUsers = (req, res) => {
-    
+    User.find()
+        .select('-__v')
+        .then((user) => {
+            res.json(user);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+};
+
+// function to get one user
+const getOneUser = (req, res) => {
+    let userID = req.params.userId;
+    User.findOne({ _id: userID })
+        .select('-__v')
+        .populate('friends')
+        .populate('thoughts')
+        .then((user) => {
+            if (!user) {
+                res.status(404).json({ message: 'Cannot find user with this ID!' });
+            }
+            res.json(user);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+};
+
+// function to create a user
+const createUser = (req, res) => {
+    let body = req.body;
+    User.create(body)
+        .then((user) => {
+            res.json(user);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+};
+
+// function to update a user
+const updateUser = (req, res) => {
+    let userID = req.params.userId;
+    let body = req.body;
+    User.findOneAndUpdate(
+        { _id: userID },
+        { $set: body },
+        { runValidators: true, new: true }
+    )
+    .then((user) => {
+        if (!user) {
+                res.status(404).json({ message: 'Cannot find user with this ID!' });
+            }
+            res.json(user);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+};
+
+const deleteUser = (req, res) => {
+    let userID = req.params.userId;
+    User.findOneAndDelete({ _id: userID })
+        .then((user) => {
+            if (!user) {
+                res.status(404).json({ message: 'Cannot find user with this ID!' });
+            }
+
+            Thought.deleteMany({ _id: { $in: user.thoughts }});
+        })
+        .then(() => {
+            res.json({ message: 'User and thoughts deleted!' });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+};
+
+const addFriend = (req, res) => {
+    let userID = req.params.userId;
+    let friendID = req.params.friendId;
+    User.findOneAndUpdate({ _id: userID }, { $addToSet: { friends: friendID }}, { new: true })
+        .then((user) => {
+            if (!user) {
+                res.status(404).json({ message: 'Cannot find User with this ID!' });
+            }
+
+            res.json(user);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+};
+
+const removeFriend = (req, res) => {
+    let userID = req.params.userId;
+    let friendID = req.params.friendId;
+    User.findOneAndUpdate({ _id: userID }, { $pull: { friends: friendID }}, { new: true })
+        .then((user) => {
+            if (!user) {
+                res.status(404).json({ message: 'Cannot find User with this ID!' });
+            }
+
+            res.json(user);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+}
+
+module.exports = {
+    getUsers,
+    getOneUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
 }
